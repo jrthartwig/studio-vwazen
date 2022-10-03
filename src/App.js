e1177b076d0966ce24ae2b697df2d13392b35ccd
jrthartwig <jocelynnhartwig@gmail.com>
2022-10-03 11:48:40 -0400

    feat: footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ArtistInStudio from "./ArtistInStudio";
 import ArtistsInStudioInformation from "./ArtistsInStudioInformation";
 import StudioVwazenBanner from "./StudioVwazenBanner";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <StudioVwazenBanner />
       <ArtistInStudio />
       <ArtistsInStudioInformation />
+      <Footer />
     </>
   );
 }
