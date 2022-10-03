3aec2f479ba197545215f0b350b18266b9f2a45d
jrthartwig <jocelynnhartwig@gmail.com>
2022-10-03 12:47:28 -0400

    feat: fix centering on contact and add bg color

--- src/ContactLyndy.jsx
+++ src/ContactLyndy.jsx
@@ -1,16 +1,16 @@
 export default function ContactLyndy() {
   return (
-    <div className="bg-white border-b-2 border-b-indigo-500/50">
+    <div className="bg-indigo-500 border-b-2 border-b-indigo-500">
       <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
         <div className="divide-gray-200">
-          <div className="lg:grid place-content-center">
-            <h2 className="text-2xl font-bold text-gray-900 sm:text-3xl sm:tracking-tight">
+          <div className="grid place-content-center">
+            <h2 className="text-2xl font-bold text-white sm:text-3xl sm:tracking-tight">
               Contact Studio Vwazen
             </h2>
-            <div className="mt-8 grid lg:col-span-2 lg:mt-0">
+            <div className="mt-8 grid lg:mt-0">
               <div>
-                <dl className="mt-2 text-base text-gray-500">
-                  <div>
+                <dl className="mt-2 text-base text-white">
+                  <div className="grid place-items-center">
                     <dt className="sr-only">Email</dt>
                     <dd>lyndy@vwazenstudio.com</dd>
                   </div>
