fa4a12be7bf5720f7e728c213fe6ac292a64ba2a
jrthartwig <jocelynnhartwig@gmail.com>
2022-10-07 09:21:55 -0400

    fix: footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-base text-gray-400">
-            &copy; 2022 Studio Vwazen LLC. All rights reserved.
+            &copy; 2022 Vwazen LLC. All rights reserved.
           </p>
         </div>
       </div>
